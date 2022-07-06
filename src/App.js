8492daee3a17ae4e8b6bed5346121c385a093e6b
NOURAH <nourahsaadan@gmail.com>
2022-07-06 12:28:00 +0300

    Updated the Chatbot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import Chatbot from 'react-chatbot-kit';
+
+import config from './components/chatbot/config';
+import ActionProvider from './components/chatbot/ActionProvider';
+import MessageParser from './components/chatbot/MessageParser';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ maxWidth: "300px"}}>
+        <Chatbot 
+          config={config}
+          actionProvider={ActionProvider}
+          messageParser = {MessageParser}
+        />
+      </div>
     </div>
   );
 }

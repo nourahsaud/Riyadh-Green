ce35e8f150865476e54b03cc7b5daf3194b814df
Nourah Alsaadan <84617366+nourahsaud@users.noreply.github.com>
2022-07-17 14:00:40 +0300

    Delete src/components/chatbot directory

--- src/components/chatbot/ActionProvider.js
+++
@@ -1,8 +0,0 @@
-class ActionProvider {
-    constuctor(createChatBotMessage, setStateFunc) {
-        this.createChatBotMessage = createChatBotMessage;
-        this.setState = setStateFunc;
-    }
-}
-
-export default ActionProvider;
